3dc9ce80dfe718422f6d68e07bea5b076a37026b
barkha2oo4 <jhabarkha808@gmail.com>
2025-06-18 18:42:46 +0530

    first commit

---
+++ backend/test_backend.py
@@ -0,0 +1,13 @@
+import requests
+
+def test_query(query):
+    url = 'http://127.0.0.1:5000/query'
+    response = requests.post(url, json={'query': query})
+    print(f'Query: {query}\nResponse: {response.json().get("response")}\n')
+
+if __name__ == '__main__':
+    test_query('What is the weather in London?')
+    test_query('Define serendipity')
+    test_query('Remind me to call John at 5pm')
+    test_query('Hello!')
+    test_query('Who is the president of France?')
