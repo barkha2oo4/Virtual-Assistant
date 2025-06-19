f8e62d6e149702c0f990f0edbc8b3b3f0446160c
barkha2oo4 <jhabarkha808@gmail.com>
2025-06-19 15:11:06 +0530

    `Added 5 new log entries to assistant.log`

--- backend/test_backend.py
+++ backend/test_backend.py
@@ -2,7 +2,7 @@ import requests
 
 def test_query(query):
     url = 'http://127.0.0.1:5000/query'
-    response = requests.post(url, json={'query': query})
+    response = requests.post(url, json={'input': query})
     print(f'Query: {query}\nResponse: {response.json().get("response")}\n')
 
 if __name__ == '__main__':
